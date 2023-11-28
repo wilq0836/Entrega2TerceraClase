a77af588d96da9b8eddc0b249a1aa174b9522ab7
Wilmar Alexander Quintero Montoya <111884757+wilq0836@users.noreply.github.com>
2023-11-27 21:02:48 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,59 +3,53 @@ import './App.css';
 import ConstructorProductos from './Componentes/ConstructorArticulos/Constructor';
 import ItemLisContainer from './Componentes/ComponentesPromise/ItemListContainer';
 import Footer from './Componentes/ComponentesApp/Footer';
+import React from 'react'; 
 import Etiquetas from './Componentes/ComponentesApp/Cards';
 import Navbar from './Componentes/ComponentesApp/Nabvar';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Car from './Componentes/Car';
+import { ImprimirRenglonesCarrito1 } from './Componentes/Tarjeta';
 import { VerItem } from './Componentes/PedirItemPorId/VerItem';
 import { VerItemMarca } from './Componentes/PedirItemPorMarca/VerItemMarca';
-import CarWidget from './Componentes/CarCompras/CarWidget';
-import { CartProvaider } from './Componentes/Context/CarContext';
-import { VerItemFetch } from './Componentes/ComponentesPromise/VerItemId';
+
 
 
 function App() {
 
 
+
   return (
 
     <div >
-    
-    <CartProvaider>
 
-      
       <BrowserRouter>
-    
       <Link className = "T" to='/'> <h1 className='titulo'>BIKESTWOGO</h1></Link>
-   
-     
+      
+      <Car></Car>
+    
       <Navbar></Navbar>
+    
       <Routes>
-        <Route path='/'></Route>  
-        <Route path='/Carrito' element={<CarWidget></CarWidget>}></Route>   
+        <Route path='/'></Route>
+       
+        <Route path='/Car' element={<ImprimirRenglonesCarrito1/>}></Route>
         <Route path='/VerItem/:id' element={<VerItem/>}></Route> 
         <Route path='/VerItem/Categorias/:marca' element={<VerItemMarca/>}></Route> 
         <Route path='/contenedorFetch'element = {<ItemLisContainer className='ItemListContainer' greeting={"ContenedorFetch"}></ItemLisContainer>}></Route>
-        <Route path='/VerItemFetch/:id'element = {<VerItemFetch/>}></Route>
-        <Route path='/constructor' element={<ConstructorProductos titulo={"Impresion desde app"} valorSeleccionadoLista={"todos"} propiedad={"Biblioteca"}></ConstructorProductos>}></Route>
-        <Route path='/constructor2' element={<ConstructorProductos titulo={"Impresion desde app"} valorSeleccionadoLista={"Colnago"} propiedad={"Biblioteca"}></ConstructorProductos>}></Route>
-        <Route path='/category/:categoryid' element={<ConstructorProductos titulo={"Impresion desde app"} valorSeleccionadoLista={"Scott"} propiedad={"Biblioteca"}></ConstructorProductos>}></Route>
-       
-      </Routes>
-      
-     
+        <Route path='/constructor' element={<ConstructorProductos titulo={"Impresion desde app"} valorSeleccionadoLista={"todos"}></ConstructorProductos>}></Route>
+        <Route path='/constructor2' element={<ConstructorProductos titulo={"Impresion desde app"} valorSeleccionadoLista={"Colnago"}></ConstructorProductos>}></Route>
+        <Route path='/category/:categoryid' element={<ConstructorProductos titulo={"Impresion desde app"} valorSeleccionadoLista={"Scott"}></ConstructorProductos>}></Route>
 
-    
-        <Etiquetas/>   
-      
-      
+      </Routes>
+
+       <Etiquetas/>   
       
        <Footer></Footer>
 
-      
+          
        
        </BrowserRouter>
-       
-       </CartProvaider>
+      
     </div>
   );
 }
